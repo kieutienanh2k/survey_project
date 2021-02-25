23d3fe1823d6a97c070cade1c12d68c518013f49
kieuanh <kieu.tienanh.38@gmail.com>
2021-02-25 17:56:02 +0700

    survey

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from Api.api import NewsId
+from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
-
 app.register_blueprint(NewsId)
+
+logging.basicConfig(level=logging.INFO, filename="Log/global.log")
+logging.getLogger('flask_cors').level = logging.DEBUG
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 if __name__ == "__main__":
     app.run(debug=True)
